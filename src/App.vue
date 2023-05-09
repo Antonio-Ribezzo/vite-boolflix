7c97b14204bafc947b7a9b2bb3a0c02d3da82e0c
Antonio Ribezzo <118204207+Antonio-Ribezzo@users.noreply.github.com>
2023-05-09 15:09:17 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  import headerComp from './components/headerComp.vue';
+
+  export default{
+    name: 'App',
+    components:{
+            headerComp
+        }
+  }
+
+</script>
+
+<template>
+  <headerComp/>
+
+  <h1 class="text-center">ciao</h1>
+  
+</template>
+
+<style lang="scss">
+  // importo il foglio di stile principale
+  @use './style/main.scss' as *;
+
+</style>
---
+++ src/components/headerComp.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: 'headerComp'
+    }
+</script>
+
+<template>
+    <div>
+        <h1>sono l'header</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
